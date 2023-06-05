a85ed94f75eb263d833d22635d8775511658e587
Ivan <kovtun65sergey@gmail.com>
2023-06-05 19:06:58 +0300

    added js carousel for our best sellers section

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        // margin:10,
+        nav:true,
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1100:{
+                items:3
+            }
+        }
+    });
+});
